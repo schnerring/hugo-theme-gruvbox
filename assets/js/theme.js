f180b1e9e4144603addbb2cf249cca0e06fb7f33
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-07-09 23:40:51 +0200

    Add dark mode OS and user preference

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,12 +1,33 @@
+function getTheme() {
+  if (localStorage && localStorage.getItem("theme")) {
+    // user preference
+    return localStorage.getItem("theme");
+  }
+  if (window.matchMedia) {
+    // OS preference
+    return window.matchMedia("(prefers-color-scheme: light)").matches
+      ? "light"
+      : "dark";
+  }
+  // undefined
+}
+
+function setTheme(theme) {
+  document.documentElement.setAttribute("theme", theme);
+  localStorage.setItem("theme", theme);
+}
+
 function toggleTheme(e) {
   const theme = e.currentTarget.classList.contains("light--hidden")
     ? "light"
     : "dark";
-  document.documentElement.setAttribute("theme", theme);
-  localStorage.setItem("theme", theme);
+  setTheme(theme);
 }
 
 const toggleButtons = document.querySelectorAll(".theme__btn");
 toggleButtons.forEach((btn) => {
   btn.addEventListener("click", toggleTheme);
 });
+
+const theme = getTheme();
+if (theme) setTheme(theme);
