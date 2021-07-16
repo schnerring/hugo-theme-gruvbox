4aeb3804ee5b2e615d06d8da6051bf1e32b7fb23
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-07-16 17:17:39 +0200

    Implement PurgeCSS
    Unify CSS into one bundle.css

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,36 @@
+const path = require("path");
+
 module.exports = () => ({
   plugins: [
-    require("postcss-import"),
+    require("postcss-import")({
+      path: path.posix.join(__dirname, "assets", "css"),
+    }),
     require("postcss-url")({
-      url: "inline",
+      filter: "**/typeface-*/files/*",
+      url: (asset) => {
+        return path.posix.join("fonts", path.basename(asset.pathname));
+      },
     }),
     require("postcss-nesting"),
     require("postcss-preset-env")({
       stage: 1,
     }),
-    require("cssnano")({
-      preset: "default",
-    }),
+    ...(process.env.HUGO_ENVIRONMENT === "production"
+      ? [
+          require("cssnano")({
+            preset: "default",
+          }),
+          require("@fullhuman/postcss-purgecss")({
+            content: ["./hugo_stats.json"],
+            defaultExtractor: (content) => {
+              let els = JSON.parse(content).htmlElements;
+              return els.tags.concat(els.classes, els.ids);
+            },
+            safelist: [":root", "theme"],
+            //fontFace: true, TODO
+            //variables: true, TODO
+          }),
+        ]
+      : []),
   ],
 });
